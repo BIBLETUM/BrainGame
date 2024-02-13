494aa652dd6efb393a146e567669102721f3d1dd
BIBLETUM <pasha.remizov@mail.ru>
2024-02-13 20:57:23 +0300

    ChooseLevelFragment and WelcomeFragment logic implemented

--- app/src/main/java/com/example/braingame/presentation/GameFragment.kt
+++ app/src/main/java/com/example/braingame/presentation/GameFragment.kt
@@ -1,12 +1,14 @@
 package com.example.braingame.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.braingame.R
 import com.example.braingame.databinding.FragmentGameBinding
+import com.example.braingame.domain.entity.Level
 import java.lang.RuntimeException
 
 class GameFragment : Fragment() {
@@ -14,6 +16,14 @@ class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    private lateinit var level: Level
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+        Log.d("ABOBBA", level.toString())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,5 +36,21 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun parseArgs(){
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    companion object{
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(level: Level): GameFragment{
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
