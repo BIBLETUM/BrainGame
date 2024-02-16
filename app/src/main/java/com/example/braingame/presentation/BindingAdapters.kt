2eb54c03e9782ecd575e2df70268eb07e2e34e28
BIBLETUM <pasha.remizov@mail.ru>
2024-02-16 15:34:47 +0300

    dataBinding implemented

--- app/src/main/java/com/example/braingame/presentation/BindingAdapters.kt
+++ app/src/main/java/com/example/braingame/presentation/BindingAdapters.kt
@@ -1,6 +1,11 @@
 package com.example.braingame.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.example.braingame.R
 
@@ -11,4 +16,84 @@ fun bindRequiredAnswers(textView: TextView, count: Int) {
         count
     )
     textView.text = minCountOfRightAnswersText
+}
+
+@BindingAdapter("scoredAnswers")
+fun bindScoredAnswers(textView: TextView, count: Int) {
+    val scoreText = String.format(
+        textView.context.getString(R.string.score_answers),
+        count
+    )
+    textView.text = scoreText
+}
+
+@BindingAdapter("requiredPercent")
+fun bindRequiredPercent(textView: TextView, count: Int) {
+    val minPercentText = String.format(
+        textView.context.getString(R.string.required_percentage),
+        count
+    )
+    textView.text = minPercentText
+}
+
+@BindingAdapter("scoredPercent")
+fun bindScoredPercent(textView: TextView, count: Int) {
+    val scorePercentageText = String.format(
+        textView.context.getString(R.string.score_percentage),
+        count
+    )
+    textView.text = scorePercentageText
+}
+
+@BindingAdapter("isWinner")
+fun bindIsWinner(imageView: ImageView, winner: Boolean) {
+    val resId = if (winner) {
+        R.drawable.ic_smile
+    } else {
+        R.drawable.ic_sad
+    }
+    val drawable = ContextCompat.getDrawable(imageView.context, resId)
+    imageView.background = drawable
+}
+
+@BindingAdapter("sumNumber")
+fun bindSumNumber(textView: TextView, sum: Int) {
+    textView.text = sum.toString()
+}
+
+@BindingAdapter("visibleNumber")
+fun bindVisibleNumber(textView: TextView, visibleNumber: Int) {
+    textView.text = visibleNumber.toString()
+}
+
+@BindingAdapter("enoughCount")
+fun bindEnoughCount(textView: TextView, enough: Boolean) {
+    val color = getColor(textView.context, enough)
+    textView.setTextColor(color)
+}
+
+@BindingAdapter("enoughPercent")
+fun bindEnoughPercent(progressBar: ProgressBar, enough: Boolean) {
+    val color = getColor(progressBar.context, enough)
+    progressBar.progressTintList = ColorStateList.valueOf(color)
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+    textView.setOnClickListener {
+        clickListener.onOptionClick(textView.text.toString())
+    }
+}
+
+interface OnOptionClickListener {
+    fun onOptionClick(option: String)
+}
+
+private fun getColor(context: Context, bool: Boolean): Int {
+    val colorResId = if (bool) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
+    }
+    return ContextCompat.getColor(context, colorResId)
 }
--- app/src/main/java/com/example/braingame/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/braingame/presentation/GameFinishedFragment.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.example.braingame.R
 import com.example.braingame.databinding.FragmentGameFinishedBinding
 import com.example.braingame.domain.entity.GameResult
 
@@ -46,50 +44,7 @@ class GameFinishedFragment : Fragment() {
         binding.buttonRetry.setOnClickListener {
             retryGame()
         }
-        setTexts()
-        drawFace()
-    }
-
-    private fun drawFace() {
-        val resId = if (gameResult.winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
-        val drawable = ContextCompat.getDrawable(requireContext(), resId)
-        binding.emojiResult.background = drawable
-    }
-
-    private fun setTexts() {
         binding.gameResult = gameResult
-        with(binding) {
-
-            val context = requireContext()
-
-//            val minCountOfRightAnswersText = String.format(
-//                context.getString(R.string.required_score),
-//                gameResult.gameSettings.minCountOfRightAnswers
-//            )
-//            tvRequiredAnswers.text = minCountOfRightAnswersText
-//
-//            val scoreText = String.format(
-//                context.getString(R.string.score_answers),
-//                gameResult.countOfRightAnswers
-//            )
-//            tvScoreAnswers.text = scoreText
-//
-//            val minPercentText = String.format(
-//                context.getString(R.string.required_percentage),
-//                gameResult.gameSettings.minPercentOfRightAnswers
-//            )
-//            tvRequiredPercentage.text = minPercentText
-
-//            val scorePercentageText = String.format(
-//                context.getString(R.string.score_percentage),
-//                gameResult.scorePercent
-//            )
-//            tvScorePercentage.text = scorePercentageText
-        }
     }
 
     private fun retryGame() {
